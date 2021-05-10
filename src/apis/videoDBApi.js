415c6cf4da25dead3ccebc37c2c9c730390978e5
Scott Carruthers <Iilluminorowebdev@gmail.com>
2021-05-10 15:30:46 -0400

    adding prox to package.json

--- src/apis/videoDBApi.js
+++ src/apis/videoDBApi.js
@@ -3,5 +3,5 @@ import axios from "axios"
 export default axios.create({
     // baseURL: "http://3.93.43.122:8080"
     // baseURL: "http://172.31.28.184:8080"
-    baseURL: ""
+    baseURL: "/"
 })
