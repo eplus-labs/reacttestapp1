5455deec257c6caf837a67b1a0dedb315e99d2ac
Scott Carruthers <Iilluminorowebdev@gmail.com>
2021-05-10 14:18:19 -0400

    cors change

--- src/apis/videoDBApi.js
+++ src/apis/videoDBApi.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL: "http://3.93.43.122:8080"
+    // baseURL: "http://3.93.43.122:8080"
     // baseURL: "http://172.31.28.184:8080"
-    // baseURL: "http://127.0.0.1:8080"
+    baseURL: "http://127.0.0.1:8080"
 })
