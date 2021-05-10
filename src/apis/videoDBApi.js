e8e6b8528e47a17886623d2f04190d0f8066c11d
Scott Carruthers <Iilluminorowebdev@gmail.com>
2021-05-10 11:34:10 -0400

    change to video DB api to use EC2 private IP address

--- src/apis/videoDBApi.js
+++ src/apis/videoDBApi.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL: "http://3.93.43.122:8080"
+    // baseURL: "http://3.93.43.122:8080"
+    baseURL: "http://172.31.28.184:8080"
     // baseURL: "http://localhost:8080"
 })
