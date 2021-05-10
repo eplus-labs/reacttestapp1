50f4404333ae5f03906dd177b37bd17e56da0f7f
Scott Carruthers <Iilluminorowebdev@gmail.com>
2021-05-10 11:41:24 -0400

    change to video DB API to 127.0.0.1

--- src/apis/videoDBApi.js
+++ src/apis/videoDBApi.js
@@ -2,6 +2,6 @@ import axios from "axios"
 
 export default axios.create({
     // baseURL: "http://3.93.43.122:8080"
-    baseURL: "http://172.31.28.184:8080"
-    // baseURL: "http://localhost:8080"
+    // baseURL: "http://172.31.28.184:8080"
+    baseURL: "http://127.0.0.1:8080"
 })
