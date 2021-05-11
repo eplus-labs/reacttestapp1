9a85c0b05cfb5a30f977769dd35e28777af937f1
Scott Carruthers <Iilluminorowebdev@gmail.com>
2021-05-10 21:50:30 -0400

    edits following move of Go API server

--- src/apis/videoDBApi.js
+++ src/apis/videoDBApi.js
@@ -1,7 +1,5 @@
 import axios from "axios"
 
 export default axios.create({
-    // baseURL: "http://3.93.43.122:8080"
-    // baseURL: "http://172.31.28.184:8080"
-    baseURL: "/"
+    baseURL: "http://44.192.44.170:8080"
 })
