a5a837cc281b0305c43d41931284d815dfb75a10
Scott Carruthers <Iilluminorowebdev@gmail.com>
2021-06-19 09:30:09 -0400

    edit of videoapi url

--- src/apis/videoDBApi.js
+++ src/apis/videoDBApi.js
@@ -1,5 +1,7 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL: "http://18.208.179.111:8080"
+    // baseURL: "http://18.208.179.111:8080"
+    baseURL: "https://videoapi.chainzero.io:8080"
+
 })
